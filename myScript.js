f0f14e41c80629c28cb7cbd5eab5bbdb16d9692e
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-12-19 16:58:20 +0530

    added operate function
    added operate function which will be use to use different calculator calculation functions

--- myScript.js
+++ myScript.js
@@ -1,3 +1,7 @@
+let number1;
+let operation;
+let number2;
+
 function add(num1, nmu2) {
     return num1 + nmu2;
 }
@@ -12,4 +16,8 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2;
+}
+
+function operate(num1, num2, operator) {
+    return operator(num1, num2);
 }
