0131276a417ab3600d7e7489487744b968438e1c
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-12-20 18:44:52 +0530

    work on calculator logic

--- myScript.js
+++ myScript.js
@@ -1,6 +1,6 @@
-let number1;
-let operation;
-let number2;
+let number1 = "";
+let operation = "";
+let number2 = "";
 
 function add(num1, nmu2) {
     return num1 + nmu2;
@@ -21,3 +21,39 @@ function divide(num1, num2) {
 function operate(num1, num2, operator) {
     return operator(num1, num2);
 }
+
+const displayResult = document.querySelector(".display-result");
+
+const buttons = document.querySelectorAll(".btn");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        if ( operation === "") {
+            number1 = Number(button.textContent);
+        } else {
+            number2 = Number(button.textContent);
+        }
+        if (button.textContent === "X") {
+            operation = multiply;
+        } else if (button.textContent === "/") {
+            operation = divide;
+        } else if (button.textContent === "-") {
+            operation = subtract;
+        } else if (button.textContent === "+") {
+            operation = add;
+        }
+        
+        if (button.textContent === "AC" || button.textContent === "clear") {
+            displayResult.textContent = "0";
+            operation = "";
+        } /*else {
+            displayResult.textContent = `${number1} ${operation} ${number2}`;
+        }*/
+        if (button.value === "equal") {
+            console.log(button.textContent);
+           console.log(operate(number1, number2, operation));
+        }
+        console.log(button.value);
+        
+    });
+});
